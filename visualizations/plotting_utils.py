d41365e1f2ba02911d950057bb528efe8b4548ce
Nalini Singh <nmsingh@mit.edu>
2021-08-27 14:24:16 -0400

    Add initial visualization utils

---
+++ visualizations/plotting_utils.py
@@ -0,0 +1,52 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+from interlacer import utils
+
+def plot_k(k,i,vmin=None,vmax=None, ax=None):
+    if(ax is not None):
+        ax.imshow(np.log(np.abs(utils.join_reim(k))+1e-8)[i,:,:],cmap='gray',vmin=vmin,vmax=vmax)
+        ax.axis('off')
+    else:
+        plt.figure()
+        plt.imshow(np.log(np.abs(utils.join_reim(k))+1e-8)[i,:,:],cmap='gray',vmin=vmin,vmax=vmax)
+        plt.axis('off')
+    
+def plot_img(img,i,vmin=None,vmax=None, ax=None):
+    if(ax is not None):
+        ax.imshow(np.abs(utils.join_reim(img))[i,:,:],cmap='gray',vmin=vmin,vmax=vmax)
+        ax.axis('off')
+    else:
+        plt.figure()
+        plt.imshow(np.abs(utils.join_reim(img))[i,:,:],cmap='gray',vmin=vmin,vmax=vmax)
+        plt.axis('off')
+    
+def plot_img_diff(img1,img2,i, ax=None):
+    if(ax is not None):
+        ax.imshow((np.abs(utils.join_reim(img1))-np.abs(utils.join_reim(img2)))[i,:,:],cmap='seismic',vmin=-1,vmax=1)
+        ax.axis('off')
+    else:
+        plt.figure()
+        plt.imshow((np.abs(utils.join_reim(img1))-np.abs(utils.join_reim(img2)))[i,:,:],cmap='seismic',vmin=-1,vmax=1)
+        plt.xticks([])
+        plt.yticks([])
+        
+        
+def plot_k_diff(k1,k2,i, ax=None):
+    if(ax is not None):
+        ax.imshow(np.log(1e-7+np.abs(utils.join_reim(k1)-utils.join_reim(k2)))[i,:,:],cmap='seismic')
+        ax.axis('off')
+    else:
+        plt.figure()
+        plt.imshow(np.log(1e-7+np.abs(utils.join_reim(k1)-utils.join_reim(k2)))[i,:,:],cmap='seismic')
+        plt.xticks([])
+        plt.yticks([])
+    
+def plot_img_from_k(k,i,vmin=None,vmax=None, ax=None):
+    plot_img(utils.convert_to_image_domain(k),i,vmin=vmin,vmax=vmax, ax=ax)
+    
+def plot_k_from_img(img,i,vmin=None,vmax=None, ax=None):
+    plot_k(utils.convert_to_frequency_domain(img),i,vmin=vmin,vmax=vmax, ax=ax)
+    
+def plot_img_from_k_diff(k1,k2,i, ax=None):
+    plot_img_diff(utils.convert_to_image_domain(k1), utils.convert_to_image_domain(k2),i, ax=ax)
---
+++ visualizations/visualization_lib.py
@@ -0,0 +1,84 @@
+import os
+
+import numpy as np
+import pandas as pd
+import scipy as sp
+import tensorflow as tf
+from PIL import Image
+from tensorboard.backend.event_processing import event_accumulator
+from tensorflow import keras
+from tensorflow.keras import backend as K
+
+from interlacer import models
+from scripts import training_config
+
+
+def get_config_path(config_dir):
+    """
+    Args:
+      config_dir(str): Path to dir containing model config file
+    Returns:
+      Path to config file
+    """
+    config_path = [i for i in os.listdir(config_dir) if i[-4:]=='.ini'][0]    
+    return config_path
+
+
+def load_model(config_path, da=False):
+    """
+    Args:
+      config_path(str): Path to model config file
+    Returns:
+      Keras model, without loaded weights
+    """
+    exp_config = training_config.TrainingConfig(config_path)
+    exp_config.read_config()
+
+    if('FASTMRI' in exp_config.dataset):
+        n = 640
+    elif(exp_config.dataset == 'MRI'):
+        n = 256
+        
+    if(exp_config.architecture == 'CONV'):
+        model = models.get_conv_no_residual_model(
+            (n, n, 2), exp_config.nonlinearity, exp_config.kernel_size, exp_config.num_features, exp_config.num_layers, exp_config.enforce_dc)
+    elif(exp_config.architecture == 'CONV_RESIDUAL'):
+        model = models.get_conv_residual_model(
+            (n, n, 2), exp_config.nonlinearity, exp_config.kernel_size, exp_config.num_features, exp_config.num_layers,exp_config.enforce_dc)
+    elif(exp_config.architecture == 'INTERLACER_RESIDUAL'):
+        model = models.get_interlacer_residual_model(
+            (n, n, 2), exp_config.nonlinearity, exp_config.kernel_size, exp_config.num_features, exp_config.num_layers,exp_config.enforce_dc)
+    elif(exp_config.architecture == 'ALTERNATING_RESIDUAL'):
+        model = models.get_alternating_residual_model(
+            (n, n, 2), exp_config.nonlinearity, exp_config.kernel_size, exp_config.num_features, exp_config.num_layers,exp_config.enforce_dc)
+
+    return exp_config, model
+
+
+def get_val_event_log(base_dir):
+    """
+    Args:
+      base_dir: Path to model training directory
+    Returns:
+      (str) Path to validation tensorboard data
+    """
+    tb_str = os.path.join(base_dir,'tensorboard/validation/')
+    return tb_str
+
+
+def get_best_ckpt(model_path):
+    """
+    Args:
+      model_path: Path to model training directory
+    Returns:
+      (int) epoch with lowest loss
+    """
+    x = event_accumulator.EventAccumulator(
+        path=get_val_event_log(model_path), size_guidance={
+            event_accumulator.SCALARS: 0}, purge_orphaned_data=False)
+    x.Reload()
+
+    img_l1 = pd.DataFrame(x.Scalars('epoch_loss')).value[100::100]
+        
+    best_epoch = 100 * (np.argmin(img_l1) + 1) # epochs stored in intervals of 100
+    return best_epoch
